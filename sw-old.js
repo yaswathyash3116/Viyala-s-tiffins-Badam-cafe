7da726b0352bafa0951b969458d4b234a6d48575
yaswathyash3116 <yaswanthyash3113@gmail.com>
2026-05-09 11:41:21 +0530

    Refactor service worker to remove caching
    Removed caching logic and disabled old cache handling.

--- sw-old.js
+++ sw-old.js
@@ -1,37 +1,18 @@
-const CACHE_NAME = 'viyalas-cache-v2';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/images/logo.png',
-  '/images/hero_food.png',
-  '/images/badam_milk.png',
-  '/images/tiffin_plate.png'
-];
+// Disable old cache completely
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.filter(cacheName => cacheName !== CACHE_NAME).map(cacheName => caches.delete(cacheName))
-      );
+    caches.keys().then(names => {
+      return Promise.all(names.map(name => caches.delete(name)));
     })
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      // Return cached version or fetch new
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
